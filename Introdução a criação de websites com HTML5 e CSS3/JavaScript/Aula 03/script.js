8e40574cb772d01043c1315e4f7f10d59d81b3ff
filipesilva <filipepsilva@outlook.com.br>
2022-03-05 07:10:57 -0300

    Objeto e desetruturação de objeto

--- Introdução a criação de websites com HTML5 e CSS3/JavaScript/Aula 03/script.js
+++ Introdução a criação de websites com HTML5 e CSS3/JavaScript/Aula 03/script.js
@@ -5,8 +5,8 @@
 // console.log(array);*/
 
 //Pode conter outros array dentro
-let array =['string', 1, true, ['array1'], ['array2']];
-/*console.log(array[4]);*/
+/*let array =['string', 1, true, ['array1'], ['array2']];
+console.log(array[4]);*/
 
 /*array.forEach(function(item, index){console.log(item, index)});*/
 
@@ -21,8 +21,27 @@ console.log(array);*/
 /*array.splice(3,4);
 console.log(array);*/
 
-let novoArray = array.slice(0,3)
-console.log(novoArray);
+/*let novoArray = array.slice(0,3)
+console.log(novoArray);*/
+
+//Aplicando características a um objeto
+let objeto = {String: 'string', Number: 1, Boolean: true, Array: ["array"], Objectintern:{Objectintern: 'objeto interno'}};
+console.log(objeto.Objectintern);
+
+//Desestruturando um objeto
+var string = objeto.String;
+console.log(string);
+
+var arrayInterno = objeto.Array
+console.log(arrayInterno);
+
+//Outra forma de realizar desestruturação
+var {Number, Boolean, Objectintern} = objeto;
+console.log(Number, Boolean, Objectintern);
+
+
+
+
 
 
 
