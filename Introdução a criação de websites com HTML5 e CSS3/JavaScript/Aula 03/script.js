7b37e76e8302b9218747f1ab7ed05f0216430cfe
filipesilva <filipepsilva@outlook.com.br>
2022-03-05 06:48:28 -0300

    Vetores e array, manipulando arrays

--- Introdução a criação de websites com HTML5 e CSS3/JavaScript/Aula 03/script.js
+++ Introdução a criação de websites com HTML5 e CSS3/JavaScript/Aula 03/script.js
@@ -1,5 +1,33 @@
 //O que são vetores e Objetos
 
 //Como declarar um array
-let array = ['string', 1 , true]
-console.log(array);
+/* let array = ['string', 1 , true]
+// console.log(array);*/
+
+//Pode conter outros array dentro
+let array =['string', 1, true, ['array1'], ['array2']];
+/*console.log(array[4]);*/
+
+/*array.forEach(function(item, index){console.log(item, index)});*/
+
+/*array.push('novo item');
+console.log(array);*/
+
+/*array.pop();
+console.log(array);*/
+
+/*console.log(array.indexOf(true));*/
+
+/*array.splice(3,4);
+console.log(array);*/
+
+let novoArray = array.slice(0,3)
+console.log(novoArray);
+
+
+
+
+
+
+
+
