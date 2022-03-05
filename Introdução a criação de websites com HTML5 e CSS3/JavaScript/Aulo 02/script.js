4228868057ecb02aacdb02e0079203a4ac427340
filipesilva <filipepsilva@outlook.com.br>
2022-03-05 05:30:15 -0300

    Aula 02 Variáveis, atribuições, operadores aritméticos, relacionais e lógicos;

---
+++ Introdução a criação de websites com HTML5 e CSS3/JavaScript/Aulo 02/script.js
@@ -0,0 +1,58 @@
+// Tipos primitivos
+
+//bollean
+var VouF = false;
+console.log(typeof(VouF));
+
+//number
+var QualquerNumero = 21
+console.log(typeof(QualquerNumero));
+
+//string
+var carateres = 'filipe'
+console.log(typeof(carateres));
+
+//Declarando variáveis
+
+var variavel = 'Filipe'
+console.log(variavel);
+
+//Variáveis de escolo global e Local
+
+var escopoglobal  = 'escopo';
+console.log(escopoglobal);
+
+function escopoLocal ( ){
+    let escopoLocalInterno = 'local'
+    console.log(escopoLocalInterno);
+}
+
+//Atribuição 
+var atribuição = 'Filipe'
+
+//Comparação, Compara valor;
+var comparação = '0' ==0
+console.log(comparação);
+
+//Comparação idêntica, compara valor das variáveis e seu tipo
+var comparaçãoIdentica = '0' ===0
+console.log(comparaçãoIdentica); 
+
+//Operadores aritméticos
+var adicao = 2+2;
+console.log(adicao);
+
+//Operadores relacionais
+var maiorQue = 3>4;
+console.log(maiorQue);
+
+//Operadores lógicos
+var e =  true && false;
+console.log(e);
+
+
+
+
+
+
+
