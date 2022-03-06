fd65adbdd0520adc6f8927ce63ab693c73cbbe56
filipesilva <filipepsilva@outlook.com.br>
2022-03-06 17:04:17 -0300

    Criação de Calculadora no Javascript

--- Introdução a criação de websites com HTML5 e CSS3/JavaScript/Aula 05/script.js
+++ Introdução a criação de websites com HTML5 e CSS3/JavaScript/Aula 05/script.js
@@ -1,11 +1,25 @@
+//Funções Declarativas
 /*function funcao () {
     console.log ('Essa mensagem veio de uma função');
 }
 
 funcao();*/
 
-function mensagem (parametro1, parametro2) {
+/*function mensagem (parametro1, parametro2) {
     console.log(parametro1, parametro2)
 }
 
-mensagem('Tudo certo' , 'jovem!');
+mensagem('Tudo certo' , 'jovem!');*/
+
+//Expressões de funções
+var Filipe = function () {
+    console.log ('Eu estou aprendendo a programar');
+}
+Filipe();
+
+//Arrow function
+
+var funcao = () => {
+    console.log ('Eu estou aprendendo funções dentro do JavaScript')
+}
+funcao();
