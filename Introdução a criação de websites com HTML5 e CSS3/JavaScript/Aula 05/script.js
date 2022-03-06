6e5859e7e52bdcd40f14d6cf498216d0c9190c2b
filipesilva <filipepsilva@outlook.com.br>
2022-03-06 15:54:10 -0300

    introdução a funções

---
+++ Introdução a criação de websites com HTML5 e CSS3/JavaScript/Aula 05/script.js
@@ -0,0 +1,11 @@
+/*function funcao () {
+    console.log ('Essa mensagem veio de uma função');
+}
+
+funcao();*/
+
+function mensagem (parametro1, parametro2) {
+    console.log(parametro1, parametro2)
+}
+
+mensagem('Tudo certo' , 'jovem!');
