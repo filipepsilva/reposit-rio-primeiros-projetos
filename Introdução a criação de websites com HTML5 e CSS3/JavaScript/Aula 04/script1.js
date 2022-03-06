a62643804972584ffbe2bc97cd00e31568712370
filipesilva <filipepsilva@outlook.com.br>
2022-03-06 15:43:55 -0300

    Inserção de estruturas de repetição

---
+++ Introdução a criação de websites com HTML5 e CSS3/JavaScript/Aula 04/script1.js
@@ -0,0 +1,34 @@
+/*let array = ['valorfilipe' ,  'valor2' ,  'valor3' , 'valor4' , 'valor5'];
+let object = {  propriedade1: 'valor1' , propriedade2: 'valor2' , propriedade3: 'valor3' ,}*/
+
+//For - executa uma instrução até que ela seja falsa
+
+/*for (let indice = 0; indice < array.length; indice++) {
+    console.log(indice);
+}*/
+
+/*for (let i in array) {
+    console.log (i);
+}
+
+for (i in object) {
+    console.log (i);
+}*/
+
+/*for (let i of array) {
+    console.log(i);
+}*/
+
+//Uso do while
+var a =0;
+/*while (a < 10) {
+    a++;
+    console.log (a);
+}*/
+
+do {
+    a++;
+    console.log(a);
+}while (a<110);
+
+
